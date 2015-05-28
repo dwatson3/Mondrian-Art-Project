703a2db343cfeb3d2e9edd7f483449436cf46c0a
Daphne Watson <daphne.renee.laurels@gmail.com>
2015-05-27 18:09:59 -0700

    First button works

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.querySelector("#painting").classList.add("animate")
+// document.querySelector("#painting").classList.remove("animate")
+
+document.querySelector("#hella_original").addEventListener("click", function() {
+// console.log("clicked");
+	document.querySelector("#painting").classList.toggle("animate")
+})
