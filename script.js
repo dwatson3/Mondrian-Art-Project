c85897aec920b4c1776ebd922de221af0764f7a1
Daphne Watson <daphne.renee.laurels@gmail.com>
2015-05-28 14:38:21 -0700

    the final three are working

--- script.js
+++ script.js
@@ -10,23 +10,75 @@ var painting = document.querySelector("#painting")
 
 document.querySelector("#hella_original").addEventListener("click", function() {
 // console.log("clicked");
-	painting.className = ''
-	painting.classList.add("animate")
-	painting.classList.add("animate-original")
+	painting.className = '';
+	painting.classList.add("animate");
+	painting.classList.add("animate-original");
+	emptyBackgroundImage();
+	emptyVideo();
 })
 
 document.querySelector("#hella_tropical").addEventListener("click", function() {
 // console.log("clicked");
 	painting.className = '' // reset
-	painting.classList.add("animate")
-	// document.querySelector("#painting").classList.add("animate")
-	painting.classList.add("animate-tropical")
+	painting.classList.add("animate");
+	painting.classList.add("animate-tropical");
+	emptyBackgroundImage();
+	emptyVideo();
 })
 
-document.querySelector("#wild_card").addEventListener("click", function() {
-// console.log("clicked");
+document.querySelector("#wild-card").addEventListener("click", function() {
 	painting.className = '' // reset
-	painting.classList.add("animate")
-	// document.querySelector("#painting").classList.add("animate")
-	painting.classList.add("animate-wild_card")
+	painting.classList.add("square");
+	wildCardGallery();
+	youtubeVideo();
 })
+
+	// painting.classList.remove("square")[i]
+
+
+// document.querySelector("#wild-card").addEventListener("click", function() {
+// // console.log("clicked");
+// 	painting.className = '' // reset
+// 	painting.add(wildCardGallery()
+// 	// document.querySelector("#painting").classList.add("animate")
+// 	// painting.classList.add("animate-wild-card")
+// 	// wildCardGallery();
+// })
+
+function youtubeVideo() {
+	var newVideo = document.createElement("iframe");
+	  newVideo.classList.add("test")
+		newVideo.src = "https://www.youtube.com/embed/BpNcmh3rxko";
+		newVideo.height = "380px";
+		newVideo.width = "300px";
+	var sweetVideo = document.getElementById("sweet-video");
+	console.log(newVideo)
+	sweetVideo.appendChild(newVideo);
+};
+
+function emptyVideo() {
+	var removeVideo = document.getElementById("sweet-video");
+	removeVideo.innerHTML = "";
+}
+
+function wildCardGallery() {
+	var grabThosePics = document.querySelectorAll(".square");
+		for (var i = 1; i < grabThosePics.length; i++) {
+			var string = "img" + i + ".jpg"
+			grabThosePics[i].style.backgroundImage = "url(" + string + ")";
+			grabThosePics[i].classList.add("final")
+	}
+}
+
+function emptyBackgroundImage() {
+	var grabThosePics = document.querySelectorAll(".square");
+		for (var i = 0; i < grabThosePics.length; i++) {
+			// grabThosePics[i].style.backgroundImage = "url(" + string + ")";						
+			grabThosePics[i].style.backgroundImage = "";	
+			// grabThosePics[i].classList.add("final")
+	}
+}
+
+
+
+
